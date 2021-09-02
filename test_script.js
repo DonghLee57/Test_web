7652e30d0850a7353bf19a99a57ac093176ff734
Dongheon <zwqa1234@gmail.com>
2021-09-02 23:32:31 +0900

    Add files via upload

--- test_script.js
+++ test_script.js
@@ -1,6 +1,9 @@
-window.onload = function(){
-    var hw = document.getElementById('hw');
-    hw.addEventListener('click', function(){
-        alert('Hello world');
-    })
-}
+<script type="text/javascript">
+function execute() {
+  var items = [[1, 2, 2],
+                  [3, 4, 3],
+                  [5, 6, 0]];
+  var out = document.getElementsByTagName('t_array');
+  out.innerHTML = items;
+  out.innerHTML = "이건 나와?";
+};
